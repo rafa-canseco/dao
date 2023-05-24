e07ea2dd2a64769026be3eca98f097bbbd9bd47b
rafa <rafa.canseco@gmail.com>
2023-05-23 19:32:06 -0600

    first commit

---
+++ backend/constants.js
@@ -0,0 +1,5 @@
+// Replace the value with your NFT contract address
+const CRYPTODEVS_NFT_CONTRACT_ADDRESS =
+  "0x21B61C9BA5f833Cba34243974834C2a6354B09D9";
+
+module.exports = { CRYPTODEVS_NFT_CONTRACT_ADDRESS };
